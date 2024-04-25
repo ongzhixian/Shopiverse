ea36c78618e8d5901809d798666b80edee594462
zhixian <zhixian@hotmail.com>
2024-04-25 22:42:54 +0800

    Add Xunit test project

--- Shopiverse.WebApi/Controllers/WeatherForecastController.cs
+++ Shopiverse.WebApi/Controllers/WeatherForecastController.cs
@@ -21,6 +21,8 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+        this._logger.LogInformation("Get called");
+
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
---
+++ Shopiverse.WebApiTests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+
+using NSubstitute;
+
+using Xunit;
+
+namespace Shopiverse.WebApi.Controllers.Tests;
+
+public class WeatherForecastControllerTests
+{
+    [Fact()]
+    public void WeatherForecastControllerTest()
+    {
+        var logger = NSubstitute.Substitute.For<ILogger<WeatherForecastController>>();
+
+        var controller = new WeatherForecastController(logger);
+
+        Xunit.Assert.NotNull(controller);
+    }
+
+    [Fact()]
+    public void GetTest()
+    {
+        var logger = NSubstitute.Substitute.For<ILogger<WeatherForecastController>>();
+
+        var controller = new WeatherForecastController(logger);
+
+        var forecasts = controller.Get();
+
+        logger.Received().Log(
+            LogLevel.Information,
+            Arg.Any<EventId>(),
+            Arg.Is<object>(o => o.ToString() == "Get called"),
+            null,
+            Arg.Any<Func<object, Exception?, string>>());
+    }
+}
